cadcfa332f1877f5f6a2eb7df4001f75a556a585
jhsebastian <jhsebastian12@gmail.com>
2020-07-01 16:23:27 -0500

    corregir el contenedor principal

--- project-bootcamp/src/containers/App.js
+++ project-bootcamp/src/containers/App.js
@@ -6,7 +6,6 @@ import Container from '../shared/Container';
 function App() {
   return (
     <div>
-      <h1>Hello! Welcome to the project, here we'll start to build an amazing web app :*</h1>
       <Logo />
       <Container />
       </div>
--- project-bootcamp/src/index.js
+++ project-bootcamp/src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <App />, document.getElementById('root')
 );
 
 
