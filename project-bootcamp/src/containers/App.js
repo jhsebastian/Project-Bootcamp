a7c995febbaf482a90bdb994fec6da638db7aee5
Anndie Gonzalez <anndiegonzalez@gmail.com>
2020-07-01 15:51:14 -0500

    se incluye el logo central

---
+++ project-bootcamp/src/containers/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Logo from '../shared/Logo';
+import Container from '../shared/Container';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello! Welcome to the project, here we'll start to build an amazing web app :*</h1>
+      <Logo />
+      <Container />
+      </div>
+  );
+}
+
+export default App;
