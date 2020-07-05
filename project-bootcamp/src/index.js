77740771bdf86af16f86d260062d942ed20c18a3
Anndie Gonzalez <anndiegonzalez@gmail.com>
2020-07-05 12:45:17 -0500

    changes-logo

--- project-bootcamp/src/index.js
+++ project-bootcamp/src/index.js
@@ -6,5 +6,3 @@ import App from './containers/App';
 ReactDOM.render(
   <App />, document.getElementById('root')
 );
-
-
--- project-bootcamp/src/shared/Logo/index.js
+++ project-bootcamp/src/shared/Logo/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Logo.css';
-import logo from '../../assets/LOGO SOLO EN JPG.jpg'
+import logo from '../../assets/Logo.jpg';
 
 class Logo extends React.Component {
     render() {
