c7e79c54953904ca1f025f15746c9cc5f821f0c0
unknown <lizethb.orozco@gmail.com>
2020-07-08 16:43:52 -0500

    add Bootstrap y Responsive logo

--- project-bootcamp/src/pages/Home/components/Logo/Logo.js
+++ project-bootcamp/src/pages/Home/components/Logo/Logo.js
@@ -6,7 +6,7 @@ class Logo extends React.Component {
     render() {
         return (
             <div className="logo">
-                <img src={logo} alt="logo" />
+                <img src={logo} class="img-fluid" alt="Responsive image"/>
             </div>
         );
     }
---
+++ project-bootcamp/src/shared/Banner/Banner.js
@@ -0,0 +1,14 @@
+import React from  "react";
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/styles.css';
+import './Banner.css';
+
+const Banner = ({ slider }) => 
+(
+  <AwesomeSlider>
+    <div className="Imagen-Banner" data-src={'/imagenes/banner1.jpg'} /> 
+    <div className="Imagen-Banner" data-src={'/imagenes/banner2.jpg'} />  
+  </AwesomeSlider>
+);
+
+export default Banner;
