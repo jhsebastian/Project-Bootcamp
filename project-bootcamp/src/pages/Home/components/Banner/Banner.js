abec296e911d2bea3898f4d12e2a3629b5e8f88e
unknown <lizethb.orozco@gmail.com>
2020-07-09 01:15:30 -0500

    Responsive Banner

--- project-bootcamp/src/pages/Home/components/Banner/Banner.js
+++ project-bootcamp/src/pages/Home/components/Banner/Banner.js
@@ -1,13 +1,28 @@
 import React from  "react";
-import AwesomeSlider from 'react-awesome-slider';
 import 'react-awesome-slider/dist/styles.css';
-import './Banner.css';
 
-const Banner = ({ slider }) => 
+const Banner = ( ) => 
 (
-  <AwesomeSlider>
-    <div className="Imagen-Banner" data-src={'/imagenes/banner1.jpg'} /> 
-    <div className="Imagen-Banner" data-src={'/imagenes/banner2.jpg'} />  
-  </AwesomeSlider>
+  <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src={'/imagenes/banner1.jpg'} className="d-block w-100" alt="..."/>
+    </div>
+    <div className="carousel-item">
+      <img src={'/imagenes/banner2.jpg'} className="d-block w-100" alt="..."/>
+    </div>
+    <div className="carousel-item">
+      <img src={'/imagenes/banner3.jpeg'} className="d-block w-100" alt="..."/>
+    </div>
+  </div>
+  <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="sr-only">Previous</span>
+  </a>
+  <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="sr-only">Next</span>
+  </a>
+</div>
 );
 export default Banner;
