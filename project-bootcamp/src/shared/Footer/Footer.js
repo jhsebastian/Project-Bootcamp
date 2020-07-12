6440680311672ed028afeaa4a82317d6190bef8e
jhsebastian <jhsebastian12@gmail.com>
2020-07-11 19:46:06 -0500

    añadir nuevos estilos al footer

--- project-bootcamp/src/shared/Footer/Footer.js
+++ project-bootcamp/src/shared/Footer/Footer.js
@@ -4,38 +4,27 @@ import './Footer.css';
 const Footer = () => {
   return(
     <div className="container">
-      <footer class="">
-          <div class="">
-            <h1 class="text-left text-uppercase font-weight-bolder">Oficinas</h1>
-            <article class="">
-              <h4 class="text-uppercase text-left font-weight-bold">Cali - Colombia</h4>
-              <span class="f">Carrera 18A 13- 37 </span>
-              <p class="" title="Call Tokyo Office">
-                312 232 8565 - 312 223 7998
-              </p>
-            </article>
-          </div>
-          <section class="">
-            <a class="" href="mailto:mediosdigitales@ccmcreactivos.com" >
-              mediosdigitales@ccmcreactivos.com
-            </a>
-            <div class="" >
-              <form class="form-inline">
-                <div class="form-group mb-2">
-                  <label for="staticEmail2" class="sr-only">Email</label>
-                  <input type="text" readonly class="form-control" id="staticEmail2" value="email@example.com"/>
-                </div>
-                <button type="submit" class="btn btn-primary mb-2">Enviar</button>
-              </form>
+      <footer class="footer-container">
+        <div>
+          <h1 class="text-left text-uppercase font-weight-bolder">CCM Creactivos</h1>
+          <h4 class="text-uppercase text-left font-weight-bold">Cali - Colombia</h4>
+          <p class="">Copyright © CCM CREACTIVOS</p>
+          
+          <form class="form-inline form-container">
+            <div class="form-group mb-2">
+              <input class="input-email" type="text" readonly class="form-control" id="staticEmail2" value="email@example.com"/>
             </div>
-          </section>
-          <div class="" >
-            <div class="">
-              <p class="">
-                Copyright © CCM CREACTIVOS
-              </p>
-            </div>
-          </div>
+            <button type="submit" class="btn btn-primary mb-2">Enviar</button>
+          </form>
+        </div>
+        <div className="footer-container-derecha">
+          <p class="">Carrera 18A 13- 37 </p>
+          <p class="">+57 312 232 8565</p>
+          <p className="">+57 312 223 7998</p>
+          <p className="">
+            <a class="footer-email text-right" href="mailto:mediosdigitales@ccmcreactivos.com">mediosdigitales@ccmcreactivos.com</a>
+          </p>
+        </div>
         </footer>
     </div>
   )
